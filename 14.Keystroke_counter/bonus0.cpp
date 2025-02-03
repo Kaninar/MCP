080668c297bb02f87e72af7bad65cec3b1f23415
Николай Ермаков <nikolaiwitch2018@gmail.com>
2025-02-03 14:53:18 +0500

    Experement 14 uploaded

--- 14.Keystroke_counter/bonus0.cpp
+++ 14.Keystroke_counter/bonus0.cpp
@@ -1,3 +1,54 @@
 #include <Arduino.h>
 // 1.Заставьте shiftOut() отправлять биты, начиная со старшего, и измените код так, чтобы счетчик по-прежнему показывал арабские цифры.
-//TODO
+#define DATA_PIN 13  // пин данных (англ. data)
+#define LATCH_PIN 12 // пин строба (англ. latch)
+#define CLOCK_PIN 11 // пин такта (англ. clock)
+#define BUTTON_PIN 10
+
+int clicks = 0;
+boolean buttonWasUp = true;
+byte segments[] = {
+    0b00111111, // 0
+    0b00000110, // 1
+    0b01011011, // 2
+    0b01001111, // 3
+    0b01100110, // 4
+    0b01101101, // 5
+    0b01111101, // 6
+    0b00000111, // 7
+    0b01111111, // 8
+    0b01101111, // 9
+};
+void setup()
+{
+    pinMode(DATA_PIN, OUTPUT);
+    pinMode(CLOCK_PIN, OUTPUT);
+    pinMode(LATCH_PIN, OUTPUT);
+    pinMode(BUTTON_PIN, INPUT_PULLUP);
+}
+
+byte reverse(byte b)
+{
+    b = (b & 0xF0) >> 4 | (b & 0x0F) << 4;
+    b = (b & 0xCC) >> 2 | (b & 0x33) << 2;
+    b = (b & 0xAA) >> 1 | (b & 0x55) << 1;
+    return b;
+}
+
+void loop()
+{
+    if (buttonWasUp && !digitalRead(BUTTON_PIN))
+    {
+        delay(10);
+        if (!digitalRead(BUTTON_PIN))
+            clicks = (clicks + 1) % 10;
+    }
+
+    buttonWasUp = digitalRead(BUTTON_PIN);
+
+    digitalWrite(LATCH_PIN, LOW);
+    // bitOrder заменил на MSBFIRST(most significant bit first) — начиная со старшего
+    shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, reverse(segments[clicks]));
+
+    digitalWrite(LATCH_PIN, HIGH);
+}
--- 14.Keystroke_counter/bonus1.cpp
+++ 14.Keystroke_counter/bonus1.cpp
@@ -1,3 +1,40 @@
 #include <Arduino.h>
 // 2.Замените кнопку на датчик света (фоторезистор в схеме делителя напряжения) и переделайте программу так, чтобы индикатор цифрой показывал уровень освещенности.
-//TODO
+#define DATA_PIN 13  // пин данных (англ. data)
+#define LATCH_PIN 12 // пин строба (англ. latch)
+#define CLOCK_PIN 11 // пин такта (англ. clock)
+#define LDR_PIN A0
+
+byte segments[] = {
+    0b00111111, // 0
+    0b00000110, // 1
+    0b01011011, // 2
+    0b01001111, // 3
+    0b01100110, // 4
+    0b01101101, // 5
+    0b01111101, // 6
+    0b00000111, // 7
+    0b01111111, // 8
+    0b01101111, // 9
+};
+
+void setup()
+{
+    pinMode(DATA_PIN, OUTPUT);
+    pinMode(CLOCK_PIN, OUTPUT);
+    pinMode(LATCH_PIN, OUTPUT);
+    pinMode(LDR_PIN, INPUT);
+}
+
+void loop()
+{
+    int lightness = analogRead(LDR_PIN);
+
+    int number = map(lightness, 0, 1023, 0, 9);
+
+    digitalWrite(LATCH_PIN, LOW);
+
+    shiftOut(DATA_PIN, CLOCK_PIN, LSBFIRST, segments[number]);
+
+    digitalWrite(LATCH_PIN, HIGH);
+}
