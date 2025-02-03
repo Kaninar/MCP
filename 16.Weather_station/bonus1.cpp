d0a248ebe14d183a4294e87ec73fe47c8eb65f27
Николай Ермаков <nikolaiwitch2018@gmail.com>
2025-02-03 14:55:42 +0500

    Experement 16 uploaded

--- 16.Weather_station/bonus1.cpp
+++ 16.Weather_station/bonus1.cpp
@@ -1,3 +1,33 @@
 #include <Arduino.h>
 // 2. Добавьте столбец, содержащий количество секунд, прошедших с момента запуска микроконтроллера. Можно уменьшить интервал передачи данных.
-//TODO
+#include <math.h>
+int minute = 1;
+
+#define TERMIST_B 4300
+
+#define VIN 5.0
+
+void setup()
+{
+    Serial.begin(9600);
+
+    Serial.println("Metiostation"); // добавлена эта строчка
+    Serial.println("Minute\tTemperature");
+}
+
+void loop()
+{
+    float voltage = analogRead(A0) * VIN / 1024.0;
+    float r1 = voltage / (VIN - voltage);
+
+    float temperature = 1. / (1. / (TERMIST_B)*log(r1) + 1. / (25. + 273.)) - 273;
+
+    Serial.print(minute);
+    Serial.print("\t");
+    Serial.print(temperature);
+    Serial.print("\t");
+    Serial.println(millis());
+
+    delay(10000);
+    ++minute;
+}
