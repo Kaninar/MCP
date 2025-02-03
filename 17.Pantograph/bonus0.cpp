db5114da79168412faeb75604a9379a79e0cce99
Николай Ермаков <nikolaiwitch2018@gmail.com>
2025-02-03 14:54:18 +0500

    Experement 17 uploaded

--- 17.Pantograph/bonus0.cpp
+++ 17.Pantograph/bonus0.cpp
@@ -1,3 +1,21 @@
 #include <Arduino.h>
 // 1.Измените программу так, чтобы по мере поворота ручки потенциометра, сервопривод последовательно занимал 8 положений: 45, 135, 87, 0, 65, 90, 180, 150°.
-//TODO
+#include <Servo.h>
+
+#define POT_MAX_ANGLE 270.0
+
+Servo myServo;
+
+void setup()
+{
+    myServo.attach(9);
+}
+
+void loop()
+{
+    int val = analogRead(A0);
+    int angle = int(val / 1024.0 * POT_MAX_ANGLE);
+
+    angle = constrain(angle, 0, 180);
+    myServo.write(angle);
+}
--- 17.Pantograph/bonus1.cpp
+++ 17.Pantograph/bonus1.cpp
@@ -1,3 +1,21 @@
 #include <Arduino.h>
 // 2. Предположим, что сервопривод управляет шторкой, и нам нужно поддерживать постоянное количество света в помещении. Создайте такой механизм.
-//TODO
+
+#include <Servo.h>
+
+#define POT_MAX_ANGLE 270.0 // макс. угол поворота потенциометра
+Servo myServo;
+
+void setup()
+{
+
+    myServo.attach(9);
+}
+
+void loop()
+{
+    int val = analogRead(A0);
+    int angle = map(val, 1023, 0, 0, 180);
+
+    myServo.write(angle);
+}
