63515bcc6d83a976c1bbe01fe3d9485541005920
Николай Ермаков <nikolaiwitch2018@gmail.com>
2025-02-07 01:31:14 +0500

    for loop removed from loop function

--- 9.Mixer/bonus0.ino
+++ 9.Mixer/bonus0.ino
@@ -1,35 +1,29 @@
 #include <Arduino.h>
-// 1. Внесите единственное изменение в программу, после которого максимальной скоростью вращения мотора составит половину от возможной.
+
 #define MOTOR_PIN 9
 #define FIRST_BUTTON_PIN 5
 #define BUTTON_COUNT 3
-#define SPEED_STEP (255 / 2 / (BUTTON_COUNT - 1))
+#define SPEED_STEP (255 / 2 / (BUTTON_COUNT - 1)) // шаг скорости уменьшен в 2 раза
+
+unsigned char i = 0;
 
 void setup()
 {
     pinMode(MOTOR_PIN, OUTPUT);
 
-    for (int i = 0; i < BUTTON_COUNT; ++i)
-        pinMode(i + FIRST_BUTTON_PIN, INPUT_PULLUP);
+    for (int pin = 0; pin < BUTTON_COUNT; ++pin)
+        pinMode(pin + FIRST_BUTTON_PIN, INPUT_PULLUP);
 }
 
 void loop()
 {
-    for (int i = 0; i < BUTTON_COUNT; ++i)
-    {
-        if (digitalRead(i + FIRST_BUTTON_PIN))
-            continue;
-        /*
-            Любое значение speed > 0 будет меньше в 2 раза.
 
-            Список значений согласно i:
-            - 0 -> 0;
-            - 1 -> 64;
-            - 2 -> 127.
-        */
+    if (digitalRead(i + FIRST_BUTTON_PIN))
+        continue;
 
-        int speed = i * SPEED_STEP;
+    int speed = i * SPEED_STEP;
 
-        analogWrite(MOTOR_PIN, speed);
-    }
+    analogWrite(MOTOR_PIN, speed);
+
+    ++i %= BUTTON_COUNT;
 }
--- 9.Mixer/bonus2.ino
+++ 9.Mixer/bonus2.ino
@@ -1,27 +1,28 @@
 #include <Arduino.h>
-// 3. Добавьте в схему еще одну кнопку, чтобы у миксера стало три режима. Понадобилось ли изменять что-либо в программе?
 #define MOTOR_PIN 9
 #define FIRST_BUTTON_PIN 5
-#define BUTTON_COUNT 4 // единственная необходимость - заменить константу количества кнопок с 3 на 4
+#define BUTTON_COUNT 4 // изменить кол-во кнопок с 3 на 4
 #define SPEED_STEP (255 / (BUTTON_COUNT - 1))
+
+unsigned int i = 0;
 
 void setup()
 {
     pinMode(MOTOR_PIN, OUTPUT);
 
-    for (int i = 0; i < BUTTON_COUNT; ++i)
-        pinMode(i + FIRST_BUTTON_PIN, INPUT_PULLUP);
+    for (int pin = 0; pin < BUTTON_COUNT; ++pin)
+        pinMode(pin + FIRST_BUTTON_PIN, INPUT_PULLUP);
 }
 
 void loop()
 {
-    for (int i = 0; i < BUTTON_COUNT; ++i)
-    {
-        if (digitalRead(i + FIRST_BUTTON_PIN))
-            continue;
 
-        int speed = i * SPEED_STEP;
+    if (digitalRead(i + FIRST_BUTTON_PIN))
+        continue;
 
-        analogWrite(MOTOR_PIN, speed);
-    }
+    int speed = i * SPEED_STEP;
+
+    analogWrite(MOTOR_PIN, speed);
+
+    ++i %= BUTTON_COUNT;
 }
