4c620da67f9e6ae8ee000f0fd71597c82f2b3a0c
Николай Ермаков <nikolaiwitch2018@gmail.com>
2025-02-12 20:31:54 +0500

    Experement 11 uploaded

--- 11.Lamp_with_pushbutton_control/bonus0.ino
+++ 11.Lamp_with_pushbutton_control/bonus0.ino
@@ -1,3 +1,42 @@
 #include <Arduino.h>
-// 1. Доработайте код таким образом, чтобы шаг изменения яркости настраивался в одном месте.
-//TODO
+
+#define PLUS_BUTTON_PIN 2
+#define MINUS_BUTTON_PIN 3
+#define LED_PIN 9
+
+int brightness = 100;
+bool plusUp = true;
+bool minusUp = true;
+
+bool handleClick(int buttonPin, boolean wasUp)
+{
+    bool isUp = digitalRead(buttonPin);
+
+    if (wasUp && !isUp)
+    {
+        delay(10);
+        isUp = digitalRead(buttonPin);
+
+        int delta = buttonPin == PLUS_BUTTON_PIN ? 35 : -35;
+
+        if (!isUp)
+            brightness = constrain(brightness + delta, 0, 255);
+    }
+
+    return isUp;
+}
+
+void setup()
+{
+    pinMode(LED_PIN, OUTPUT);
+    pinMode(PLUS_BUTTON_PIN, INPUT_PULLUP);
+    pinMode(MINUS_BUTTON_PIN, INPUT_PULLUP);
+}
+
+void loop()
+{
+    analogWrite(LED_PIN, brightness);
+
+    plusUp = handleClick(PLUS_BUTTON_PIN, plusUp);
+    minusUp = handleClick(MINUS_BUTTON_PIN, minusUp);
+}
--- 11.Lamp_with_pushbutton_control/bonus1.ino
+++ 11.Lamp_with_pushbutton_control/bonus1.ino
@@ -1,4 +1,50 @@
 #include <Arduino.h>
-// 2. Создайте еще одну функцию и переделайте код так, чтобы одна функция отвечала за отслеживание нажатий,
-// а другая — за вычисление яркости светодиода и возвращала его в analogWrite.
-// TODO
+
+#define PLUS_BUTTON_PIN 2
+#define MINUS_BUTTON_PIN 3
+#define LED_PIN 9
+
+const int BUTTONS[2] = {PLUS_BUTTON_PIN, MINUS_BUTTON_PIN};
+
+int brightness = 100;
+bool plusUp = true;
+bool minusUp = true;
+bool wasUp[2] = {true, true};
+
+int getBrightness(int buttonPin)
+{
+    if (!wasUp[0])
+        brightness += 35;
+
+    if (!wasUp[1])
+        brightness -= 35;
+
+    brightness = constrain(brightness, 0, 255);
+
+    return brightness;
+}
+
+void handleClick()
+{
+    for (int pin = 0; pin < 2; pin++)
+    {
+        bool isUp = digitalRead(BUTTONS[pin]);
+
+        delay(10);
+        isUp = digitalRead(BUTTONS[pin]);
+
+        wasUp[pin] = isUp;
+    }
+}
+
+void setup()
+{
+    pinMode(LED_PIN, OUTPUT);
+    pinMode(PLUS_BUTTON_PIN, INPUT_PULLUP);
+    pinMode(MINUS_BUTTON_PIN, INPUT_PULLUP);
+}
+
+void loop()
+{
+    analogWrite(LED_PIN, getBrightness());
+}
